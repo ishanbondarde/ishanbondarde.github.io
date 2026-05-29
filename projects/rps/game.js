428cea66c886cfb5fb738434107f934bfa1c8dd7
ishanbondarde <ishanbondarde25@gmail.com>
2026-05-29 13:46:48 +0530

    Update game.js

--- projects/rps/game.js
+++ projects/rps/game.js
@@ -1,7 +1,7 @@
 function Play(userChoice) {
 let choices = ["Rock","Paper","Scissor"];
 
-let randomIndex = Math.floor(Math.random() * 3);
+let randomIndex = Math.floor(Math.random()* 3);
  console.log(randomIndex);
 
 let computerChoice = choices[randomIndex];
@@ -15,15 +15,14 @@ if (userChoice === computerChoice) {
 
 else if ( (userChoice === "Rock" && computerChoice === "Scissor") || (userChoice === "Scissor" && computerChoice === "Paper") || (userChoice === "Paper" && computerChoice === "Rock") )
         {
-        result = "You Won!!!";
+        result = "Player Won!!!";
         }
         
 else {
 result = "Computer Win!!"; 
-
-
 }
-        
+   
+   
  document.getElementById("Result").innerHTML = "computer Choice : " + computerChoice + "<br>" + result  ;        
         
  }       
@@ -70,6 +69,5 @@ result = "Computer Win!!";
 
 
 
-}
 
 
